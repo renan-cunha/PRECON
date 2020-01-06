68fe54e13dc71ac112effbceb141a7bbf4e3152a
Renan Cunha <renancunhafonseca@gmail.com>
2020-01-06 17:14:08 -0300

    Fix bug, np.zeros instead of np.array

--- src/expert_dataset.py
+++ src/expert_dataset.py
@@ -10,15 +10,15 @@ def generate_expert_dataset(expert_agent: ExpertAgent, env: gym.Env,
     action_dim = env.action_space.shape[0]
     state_dim = env.observation_space.shape[0]
 
-    states = np.array((num_steps, state_dim), dtype="float32")
-    actions = np.array((num_steps, action_dim), dtype="float32")
+    states = np.zeros((num_steps, state_dim), dtype="float32")
+    actions = np.zeros((num_steps, action_dim), dtype="float32")
 
     obs = env.reset()
     for i in range(num_steps):
-        action = expert_agent.predict(obs)
+        action = expert_agent.predict(obs)[0]
 
-        states[i] = obs
-        actions[i] = action
+        states[i, :] = obs
+        actions[i, :] = action
 
         obs, _, done, _ = env.step(action)
         if done:
