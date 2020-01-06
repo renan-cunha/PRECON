ef0bc21a8a7f6e81486e0cab8ecee0ec9648daf7
Renan Cunha <renancunhafonseca@gmail.com>
2020-01-06 16:40:48 -0300

    Generate expert_dataset, Fix #12

--- src/expert_dataset.py
+++ src/expert_dataset.py
@@ -6,4 +6,22 @@ import numpy as np
 
 def generate_expert_dataset(expert_agent: ExpertAgent, env: gym.Env,
                             num_steps: int) -> Dict[str, np.ndarray]:
-    pass
+
+    action_dim = env.action_space.shape[0]
+    state_dim = env.observation_space.shape[0]
+
+    states = np.array((num_steps, state_dim), dtype="float32")
+    actions = np.array((num_steps, action_dim), dtype="float32")
+
+    obs = env.reset()
+    for i in range(num_steps):
+        action = expert_agent.predict(obs)
+
+        states[i] = obs
+        actions[i] = action
+
+        obs, _, done, _ = env.step(action)
+        if done:
+            obs = env.reset()
+
+    return {"actions": actions, "states": states}
