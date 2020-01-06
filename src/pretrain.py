6acd43ce93c05582a9512670f2edf66f13a4eaf5
Renan Cunha <renancunhafonseca@gmail.com>
2020-01-06 17:14:33 -0300

    Implement fit method of Pretrain

--- src/pretrain.py
+++ src/pretrain.py
@@ -1,35 +1,30 @@
 from stable_baselines.common.base_class import BaseRLModel
 import numpy as np
 from keras import Model
+from src.keras_from_rl import keras_from_rl
+
 
 class Pretrain:
-    def __init__(self, model: BaseRLModel, states:np.darray, 
-                 actions:np.ndarray):
+    def __init__(self, model: BaseRLModel, states: np.ndarray,
+                 actions: np.ndarray):
         self.model = model
         self.states = states
         self.actions = actions
+        self.keras_model = keras_from_rl(model)
 
-    #TODO: Verify that what output position is the stddev of the policy
-    #TODO: make dataset actions with stddev equal to 0
-    #TODO: Make clone of the model policy using keras
     #TODO: Train the cloned model
     #TODO: set the weights of the BaseRLModel
-    
-    def _add_stddev(self, actions: np.ndarray) -> np.ndarray:
-        """The dataset action has just the actual value, the pre-trained agent
-           will be trained with mean equal to those values and stddev equal to 
-           0"""
-        """CODE HERE"""
-        pass
 
-    def _clone_model(self, model: BaseRLModel) -> Model:
-        """CODE HERE"""
-        pass
+    def fit(self, num_epochs: int, batch_size=64, validation_split=0.75) -> Model:
 
-    def fit(self, num_epochs: int) -> Model:
-        pass
+        self.keras_model.compile(optimizer="adam", loss="mse",
+                                 metrics=["mse"])
+        self.keras_model.fit(self.states, self.actions,
+                             batch_size=batch_size,
+                             epochs=num_epochs,
+                             validation_split=validation_split)
 
-    def _set_weights_base(self) -> None:
+    def get_pretrained_model(self) -> BaseRLModel:
         """'Clone the model on the way Keras -> BaseRLModel"""
         pass
 
