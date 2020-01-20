bdc59f137572c959dd3bb2d3cdbbdc3a358afc99
Renan Cunha <renancunhafonseca@gmail.com>
2020-01-20 14:17:13 -0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='precon',
+      version='0.0.1',
+      description='Pre-training with continous actions',
+      author='Renan Cunha',
+      author_email='renancunhafonseca@gmail.com',
+      packages=["precon"],
+      )
